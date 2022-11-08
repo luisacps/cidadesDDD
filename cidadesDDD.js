c0d635504834d62b65bf58369cf42759d4f57778
Luísa C. Pimentel dos Santos <101952537+luisacps@users.noreply.github.com>
2022-11-07 23:18:18 -0300

    Create cidadesDDD.js

---
+++ cidadesDDD.js
@@ -0,0 +1,21 @@
+document.querySelector("#ddd").addEventListener('change', procurarAPI);
+c = new Array();
+
+function procurarAPI(algo) {
+    fetch(`https://brasilapi.com.br/api/ddd/v1/${algo.target.value}`)
+    .then(res => res.json())
+    .then (dados => {
+        c = dados.cities;
+        listarCidades();
+    });
+}
+
+function listarCidades() {
+    l = document.querySelector('ul');
+    l.textContent = '';
+    city.forEach(c => {
+        elemento = document.createElement('li');
+        elemento.textContent = c;
+        l.append(elemento);
+    });
+}
